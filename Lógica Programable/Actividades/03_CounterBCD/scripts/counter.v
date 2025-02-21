5430d46071f12ee11a38c8002b02b8b7238f9ac5
David Martinez <david.martinez141104@gmail.com>
2025-02-21 11:39:24 -0600

    Practica de contadores y BCD

---
+++ Lógica Programable/Actividades/03_CounterBCD/scripts/counter.v
@@ -0,0 +1,16 @@
+module counter #(
+    parameter OUT_WIDTH = 4
+)(
+    input clk, rst, enable,
+    output reg [OUT_WIDTH-1:0] count
+);
+    always @(posedge clk or posedge rst) 
+        begin
+            if (rst)
+                count <= 4'b0000;
+            else if (enable)
+                count <= count + 1;
+            else 
+                count <= count;
+        end
+endmodule
